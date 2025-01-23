041a2e8d31250e67652d68a070e89b2f6a977a89
Dzenoo <dzenisgudzevic18@gmail.com>
2025-01-23 21:04:14 +0100

    feat: enhance server initialization with CORS, security middleware, and global validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+import helmet from 'helmet';
+
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function initializeServer() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+
+  app.enableCors({
+    origin: ['https://jobernify.com', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(compression());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
 }
-bootstrap();
+initializeServer();
