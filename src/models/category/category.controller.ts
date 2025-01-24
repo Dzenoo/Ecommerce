368d8307cd9e5225614af51fbcd22258bbe5e970
Dzenoo <dzenisgudzevic18@gmail.com>
2025-01-24 19:40:39 +0100

    feat: implement category creation with validation and error handling

--- src/models/category/category.controller.ts
+++ src/models/category/category.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
 import { CategoryService } from './category.service';
 
-@Controller('category')
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+@Controller('/category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Post('/create')
+  async createCategory(@Body() body: CreateCategoryDto) {
+    return this.categoryService.create(body);
+  }
 }
--- src/models/category/category.service.ts
+++ src/models/category/category.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpStatus,
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+import { Category } from './schema/category.schema';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectModel(Category.name) private readonly categoryModel: Model<Category>,
+  ) {}
+
+  async create(body: CreateCategoryDto): Promise<ResponseObject> {
+    const existingCategory = await this.categoryModel.findOne({
+      name: body.name,
+    });
+
+    if (existingCategory) {
+      throw new NotAcceptableException('Category already exists');
+    }
+
+    if (body.parentCategory) {
+      const parent = await this.categoryModel.findById(body.parentCategory);
+      if (!parent) {
+        throw new NotFoundException('Parent category not found');
+      }
+    }
+
+    const newCategory = await this.categoryModel.create({
+      name: body.name,
+      parentCategory: body.parentCategory || null,
+      filters: body.filters || [],
+    });
+
+    if (!newCategory) {
+      throw new NotAcceptableException('Category not created');
+    }
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Category created successfully',
+    };
+  }
+
+  async update() {}
+
+  async delete() {}
+
+  async getAll() {}
+
+  async getOne() {}
+}
