02a5d30c8b5b7fc8bdb30162517105d7948d6215
Dzenoo <dzenisgudzevic18@gmail.com>
2025-01-24 21:57:21 +0100

    feat: implement category update and deletion functionality with validation

--- src/models/category/category.controller.ts
+++ src/models/category/category.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
 
 import { CategoryService } from './category.service';
 
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('/category')
 export class CategoryController {
@@ -11,5 +12,18 @@ export class CategoryController {
   @Post('/create')
   async createCategory(@Body() body: CreateCategoryDto) {
     return this.categoryService.create(body);
+  }
+
+  @Patch('/update/:id')
+  async updateCategory(
+    @Body() body: UpdateCategoryDto,
+    @Param('id') id: string,
+  ) {
+    return await this.categoryService.update({ body, id });
+  }
+
+  @Delete('/delete/:id')
+  async deleteCategory(@Param('id') id: string) {
+    return await this.categoryService.delete(id);
   }
 }
--- src/models/category/category.service.ts
+++ src/models/category/category.service.ts
@@ -8,6 +8,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 import { Category } from './schema/category.schema';
 
@@ -49,9 +50,56 @@ export class CategoryService {
     };
   }
 
-  async update() {}
+  async update(data: {
+    body: UpdateCategoryDto;
+    id: string;
+  }): Promise<ResponseObject> {
+    const { body, id } = data;
 
-  async delete() {}
+    const categoryExists = await this.categoryModel.findById(id);
+
+    if (!categoryExists) {
+      throw new NotAcceptableException('Category does not exist.');
+    }
+
+    const category = await this.categoryModel.findByIdAndUpdate(
+      id,
+      { $set: body },
+      {
+        new: true,
+        runValidators: true,
+        strict: true,
+      },
+    );
+
+    if (!category) {
+      throw new NotAcceptableException('Category could not be updated.');
+    }
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Category updated successfully.',
+    };
+  }
+
+  async delete(id: string) {
+    const categoryExists = await this.categoryModel.findById(id);
+
+    if (!categoryExists) {
+      throw new NotAcceptableException('Category does not exist.');
+    }
+
+    const category = await this.categoryModel.findByIdAndDelete(id);
+
+    if (!category) {
+      throw new NotAcceptableException('Category could not be deleted.');
+    }
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Category deleted successfully.',
+    };
+  }
 
   async getAll() {}
 
