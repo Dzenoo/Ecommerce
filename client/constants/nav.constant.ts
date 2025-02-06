e88a6841d1c12769b05519cf19026177204e0dd7
Dzenoo <dzenisgudzevic18@gmail.com>
2025-02-06 18:42:19 +0100

    feat: add Home action to AdminNavbar and update IDs for consistency

--- client/constants/nav.constant.ts
+++ client/constants/nav.constant.ts
@@ -8,6 +8,7 @@ import {
   TicketCheck,
   Plus,
   View,
+  Home,
 } from 'lucide-react';
 
 export const UserNavbarActions = [
@@ -34,25 +35,32 @@ export const UserNavbarActions = [
 export const AdminNavbarActions = [
   {
     id: 1,
+    icon: Home,
+    text: 'Home',
+    href: '/',
+    subActions: [],
+  },
+  {
+    id: 2,
     icon: LayoutDashboard,
     text: 'Dashboard',
     href: '/dashboard',
     subActions: [],
   },
   {
-    id: 2,
+    id: 3,
     icon: ShoppingBag,
     text: 'Products',
     href: '/dashboard/products',
     subActions: [
       {
-        id: 2.1,
+        id: 3.1,
         icon: Plus,
         text: 'Add Product',
         href: '/dashboard/products/add',
       },
       {
-        id: 2.2,
+        id: 3.2,
         icon: View,
         text: 'View Products',
         href: '/dashboard/products',
@@ -60,26 +68,26 @@ export const AdminNavbarActions = [
     ],
   },
   {
-    id: 3,
+    id: 4,
     icon: ClipboardList,
     text: 'Orders',
     href: '/dashboard/orders',
     subActions: [],
   },
   {
-    id: 4,
+    id: 5,
     icon: TicketCheck,
     text: 'Coupons',
     href: '/dashboard/coupons',
     subActions: [
       {
-        id: 4.1,
+        id: 5.1,
         icon: Plus,
         text: 'Create Coupon',
         href: '/dashboard/coupons/create',
       },
       {
-        id: 4.2,
+        id: 5.2,
         icon: View,
         text: 'View Coupons',
         href: '/dashboard/coupons/view',
@@ -87,7 +95,7 @@ export const AdminNavbarActions = [
     ],
   },
   {
-    id: 5,
+    id: 6,
     icon: Settings,
     text: 'Settings',
     href: '/dashboard/settings',
