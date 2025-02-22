d47647126899a9922d1666634d57fd6a784b5dcb
Dzenoo <dzenisgudzevic18@gmail.com>
2025-02-22 22:30:52 +0100

    feat: replace ContinueToOrder component with CartOrderDetails for enhanced order summary display

--- client/app/(user)/cart/page.tsx
+++ client/app/(user)/cart/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { CartQueryType, useCartQuery } from '@/hooks/queries/useCart.query';
 import Cart from '@/components/user/cart/Cart';
 import NotFound from '@/components/shared/NotFound';
-import ContinueToOrder from '@/components/user/cart/ContinueToOrder';
+import CartOrderDetails from '@/components/user/cart/CartOrderDetails';
 
 const CartPage = () => {
   const { data, isLoading } = useCartQuery({
@@ -27,7 +27,7 @@ const CartPage = () => {
   return (
     <section className="grid grid-cols-[2.5fr,1fr] gap-5 pt-5">
       <Cart cart={data.cart} />
-      <ContinueToOrder />
+      <CartOrderDetails cart={data.cart} />
     </section>
   );
 };
---
+++ client/components/user/cart/CartOrderDetails.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { ICart } from '@/types';
+import { getCategory } from '@/lib/utils';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/layout/card';
+import { Separator } from '@/components/ui/layout/separator';
+import { Button } from '@/components/ui/buttons/button';
+import FieldGroup from '@/helpers/FieldGroup';
+
+type CartOrderDetailsProps = {
+  cart: ICart;
+};
+
+const CartOrderDetails: React.FC<CartOrderDetailsProps> = ({ cart }) => {
+  const cartItems = cart.items.map((item) => item.product);
+  const productsUrl = cartItems.map(
+    (i) => `/products/${getCategory('id', i.category)?.name.toLowerCase()}`,
+  )[0];
+
+  return (
+    <Card className="h-fit shadow-none">
+      <CardHeader>
+        <CardTitle>Order Details</CardTitle>
+        <CardDescription>
+          Continue to checkout to complete your order.
+        </CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent className="space-y-2">
+        <FieldGroup
+          title="Subtotal"
+          value={`${cart.totalPrice} DIN`}
+          customStyles={{
+            div: 'flex flex-row justify-between',
+            p: 'font-bold text-black text-sm',
+          }}
+        />
+        <FieldGroup
+          title="Delivery"
+          value={'Shipping costs are calculated during checkout.'}
+          customStyles={{
+            div: 'flex flex-row justify-between',
+            p: ' text-sm',
+          }}
+        />
+      </CardContent>
+      <CardFooter className="flex flex-col space-y-2 pt-0">
+        <Link href="/checkout" className="w-full">
+          <Button className="w-full">Proceed to checkout</Button>
+        </Link>
+        <Link href={productsUrl} className="w-full">
+          <Button variant="outline" className="w-full">
+            Continue Shopping
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CartOrderDetails;
