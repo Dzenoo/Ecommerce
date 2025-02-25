4fd76968c69a75b57b48a9e103b24dcec8617762
Dzenis <dzenisgudzevic18@gmail.com>
2025-02-25 11:25:34 +0100

    feat: migrate from bcrypt to bcryptjs for improved compatibility

--- api/src/authentication/services/local-auth.service.ts
+++ api/src/authentication/services/local-auth.service.ts
@@ -15,7 +15,7 @@ import { BlockedDomainsService } from '@/common/services/blocked-domains.service
 import { SignupDto } from '../dto/signup.dto';
 import { getRedirectUrl } from '@/common/utils';
 
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class LocalAuthService {
